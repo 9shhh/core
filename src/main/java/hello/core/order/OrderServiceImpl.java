54e71697cf9835d39e0566b9adf6db6b91fc2322
9shhh <uelj1234@naver.com>
2022-10-07 01:46:14 +0900

    OrderServiceImpl 할인 DIP 위배 사항 변경 - 주석 내용 확인 필수!

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +8,14 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy =  new FixDiscountPolicy();
+
+    /**
+     * 아래 DiscountPolicy 에 대한 부분은 DIP는 잘 지킴 => 인터페이스를 의존하게 함.
+     * 하지만, 구현체(실제 기능을 하는 동작 객체)가 없음.
+     * TODO: 구현체 클래스를 생성하고 주입하라.
+     */
+    private DiscountPolicy discountPolicy;
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
