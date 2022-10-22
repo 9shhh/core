86dd43527b25aabefe24f7bb0919fd617ecf4020
9shhh <uelj1234@naver.com>
2022-10-23 02:54:07 +0900

    AppConfig 설정 변경 - @Configuration 정의를 통한 스프링 컨테이너 등록 - @Bean 정의를 통한 사용 객체들 스프링 컨테이너 등록 - 회원, 주문 실행 코드 변경 - 접근 제어자 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 해당 어노테이션으로 해당 클래스는 설정(구성) 정보로 스프링 컨테이너에서 사용된다.
 public class AppConfig { // 애플리케이션의 실제 동작에 필요한 *구현 객체를 생성* 한다. (의존적인 객체의 생성과 연결을 담당함.)
 
+    @Bean // 해당 어노테이션으로 해당 메소드는 현재 정의된 메소드 명으로 스프링 빈에 등록된다. @Bean(name="mmm") 형식으로 지정할 수 있지만 관례상 기본 설정을 따르는걸 권장함.
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
          // 클라이언트 코드는 변경하지 않고 config(구성) 만 변경함으로써, 할인 정책을 변경함. => 변경 최소화.
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,27 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // 순수 자바를 이용한 개발 검증 로직
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        /**
+         * 스프링은 모든것이 Application Context 라는 것으로 시작됨. -> 이게 스프링 컨테이너 라고 보면됨.
+         * 이게 스프링의 모든 객체들을 관리한다. -> AppConfig.java에 선언한 @Bean 들의 객체를 관리함.
+         */
+
+        // AppConfig.class 를 AnnotationConfigApplicationContext에 넣어 줌으로써 AppConfig의 선언된 빈들의 객체를 생성해서 관리해준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // ApplicationContext에 등록된 객체는 AppConfig에 정의된 메소드 명으로 등록이 된다. 따라서, 사용할 객체는 메소드 명으로 이름을 주고, 해당 객체의 타입을 정의한다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
