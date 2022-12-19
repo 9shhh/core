2b4d64cb81c19af9936b8eeef7dbd3aa4d386d34
9shhh <uelj1234@naver.com>
2022-12-19 22:49:27 +0900

    조회 빈 2개 이상일 때 상황 해결 - 충돌되는 빈 중 "사용할 빈"의 이름을 필드명(필드주입), 파라미터명(생성자, 수정자 등)으로 지정함.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,12 +13,14 @@ public class OrderServiceImpl implements OrderService {
 
     private final DiscountPolicy discountPolicy;
 
+    @Autowired private DiscountPolicy rateDiscountPolicy; // 필드 주입 시 필드명을 특정 빈의 이름으로 지정하면 됨.
+
      @Autowired
     // 해당 어노테이션을 통해 여러 의존관계도 자동으로 주입 받을 수 있음.
     // 생성자에 파라미터가 많아도 다 알아서 찾아 주입해줌.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
         this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
+        this.discountPolicy = rateDiscountPolicy; // 파라미터명을 주입 받을 특정 빈의 이름으로 지정하면 됨.
     }
 
     @Override
