6a0a9ccd9f5d79c789afed469a32094a5224ad40
9shhh <uelj1234@naver.com>
2022-12-19 22:55:23 +0900

    조회 빈 2개 이상일 때 상황 해결(2번째 방법) - @Qualifier 어노테이션을 사용하여 충돌되는 각 빈의 키워드를 부여하고 주입받는 필드 또는 생성자, 수정자 파라미터에 @Qualifier 로 특정 빈의 키워드를 지정함.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,14 +14,12 @@ public class OrderServiceImpl implements OrderService {
 
     private final DiscountPolicy discountPolicy;
 
-    @Autowired private DiscountPolicy rateDiscountPolicy; // 필드 주입 시 필드명을 특정 빈의 이름으로 지정하면 됨.
-
      @Autowired
     // 해당 어노테이션을 통해 여러 의존관계도 자동으로 주입 받을 수 있음.
     // 생성자에 파라미터가 많아도 다 알아서 찾아 주입해줌.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
-        this.discountPolicy = rateDiscountPolicy; // 파라미터명을 주입 받을 특정 빈의 이름으로 지정하면 됨.
+        this.discountPolicy = discountPolicy;
     }
 
     @Override
