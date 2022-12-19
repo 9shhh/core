f3be7502e2461cee8a873ad6544312de99318c29
9shhh <uelj1234@naver.com>
2022-12-19 23:03:31 +0900

    조회 빈 2개 이상일 때 상황 해결(3번째 방법) - @Primary 어노테이션을 통한 주입될 빈의 우선권 지정.

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,11 +2,11 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary // 해당 어노테이션을 통해 동일한 구현체를 가지는 빈 중에 우선권을 갖게되고 해당 객체의 빈이 주입되게 됨.
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,7 +4,6 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,7 +16,7 @@ public class OrderServiceImpl implements OrderService {
      @Autowired
     // 해당 어노테이션을 통해 여러 의존관계도 자동으로 주입 받을 수 있음.
     // 생성자에 파라미터가 많아도 다 알아서 찾아 주입해줌.
-    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
