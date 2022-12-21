4fcf385acbbf8aeade74e86ed0cd07743126e9f3
9shhh <uelj1234@naver.com>
2022-12-22 01:45:19 +0900

    스프링 빈 생명주기 콜백 실습(마지막) - @PostConstruct, @PreDestroy 어노테이션 지정하는 방법으로 테스트

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -19,25 +19,11 @@ public class BeanLifeCycleTest {
     static class LifeCycleConfig {
 
         // 빈으로 등록될 객체 내부에서 개발자가 원하는 이름으로 초기화, 소멸(종료) 메소드를 정의하고 @Bean 의 initMethod, destroyMethod 로 정의한 메소드를 지정함.
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
         }
     }
-    /**
-     * 기존 초기화, 소멸(종료) 메소드를 인터페이스를 구현하는 방식에서 설정 정보를 사용해 지정하는 방법으로 변경함.
-     * 해당 방법의 특징으로는
-     * 1. 초기화, 소멸(종료) 메소드의 이름을 자유롭게 줄 수 있음.
-     * 2. 스프링 빈이 스프링 코드에 의존하지 않음.
-     * 3. 코드가 아니라 설정 정보를 사용하기 때문에 코드를 고칠 수 없는 외부 라이브러리에도 초기화, 소멸(종료) 메소드를 적용할 수 있음.
-     *
-     * 추가로, @Bean 의 destroyMethod 속성은 특별한 기능이 있음.
-     * 라이브러리는 대부분 close, shutdown 과 같은 이름의 종료 메소드를 사용함.
-     * Bean 의 destroyMethod 속성은 메소드 명을 지정하지 않으면 기본값이 (inferred) (추론)으로 등록되어 있음.
-     * 이 추론 기능은 close, shutdown 이라는 이름의 메소드를 자동으로 호출해줌. 이름 그대로의 종료 메소드를 추론해서 호출해줌.
-     * 따라서, 직접 스프링 빈으로 등록하면 종료 메소드는 따로 적어주지 않아도 잘 동작함.
-     * 만약 해당 기능을 사용하기 싫으면, destroyMethod="" 처럼 공백을 지정하면 됨.
-     */
 }
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 // 가짜 네트워크 클라이언트
 public class NetworkClient {
 
@@ -28,14 +31,26 @@ public class NetworkClient {
         System.out.println("close = " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
     }
 }
+/**
+ * @PostConstruct, @PreDestroy 어노테이션을 사용한 초기화, 소멸(종료)를 사용.
+ * 최신 스프링에서 가장 권장하는 방법임.
+ * 어노테이션만 초기화, 소멸(종료) 메소드에 정의하면 되서 매우 편리함.
+ * import 한 패키지를 보면 javax.annotation.PostConstruct, javax.annotation.PreDestroy 로
+ * 스프링의 종속적인 기술이 아니라 JSR-250 이라는 자바 표준 기술이므로, 스프링이 아닌 다른 컨테이너에서도 동작함.
+ * 컴포넌트 스캔과 잘 어울림.
+ * 단점은, 외부 라이브러리에는 적용이 불가능함.
+ * 따라서 외부 라이브러리에 초기화, 소멸(종료) 가 필요하다면 @Bean 의 initMethod, destroyMethod 속성을 사용하자.
+ */
