4676146588500811f9bcc144ef3a24ea4e059272
9shhh <uelj1234@naver.com>
2022-12-23 02:05:14 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider 로 문제 해결 테스트 (자바 표준 JSR-330 Provider 를 사용.)

--- src/test/java/hello/core/scope/singletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/singletonWithPrototypeTest1.java
@@ -1,12 +1,12 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -42,24 +42,27 @@ public class singletonWithPrototypeTest1 {
 
     @Scope("singleton")
     static class ClientBean {
-        // ObjectProvider 를 따로 빈 등록 하지 않았지만, 스프링이 자동으로 만들어줌.
-        private final ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;
+        private final Provider<PrototypeBean> prototypeBeanObjectProvider;
 
-        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanObjectProvider) {
+        public ClientBean(Provider<PrototypeBean> prototypeBeanObjectProvider) {
             this.prototypeBeanObjectProvider = prototypeBeanObjectProvider;
         }
 
         public int logic() {
-            // getObject() 시 스프링 컨테이너에셔 프로토타입 빈을 찾아 반환해줌.
-            // ObjectProvider 스프링 컨테이너를 통한 Dependency LookUp 을 간단하게 할 수 있도록 도와주는거임. (프로토타입 전용으로 사용되는 것이 아님. 대리자 정도의 역할이라고 보면됨.)
-            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
 
         /**
-         * ObjectFactory<T>: 기능이 단순, 별도의 라이브러리 필요 없음, 스프링에 의존.
-         * ObjectProvider<T>: ObjectFactory 상속, 옵션, 스트림 처리 등 편의 기능이 많고, 별도의 라이브러리 필요 없음, 스프링에 의존.
+         * 자바에서 제공하는 Provider<T> 를 활용한 Dependency LookUp.
+         * get() 메소드 하나로 기능이 매우 단순함.
+         * 별도의 라이브러리가 필요함. (javax.inject:javax.inject:1)
+         * 자바 표준이므로 스프링이 아닌 다른 컨테이너에서도 사용할 수 있음.
+         *
+         * #정리#
+         * 프로토타입 빈을 언제 사용할까? -> 매번 사용할 때 마다 의존관계 주입이 완료된 새로운 객체가 필요할 때 사용하면 됨.
+         * 그런데 실무에서 웹 애플리케이션을 개발하다보면, 싱글톤 빈으로 대부분의 문제를 해결 할 수 있기 때문에 프로토타입 빈을 직접적으로 사용하는 일은 매우 드뭄.
          */
     }
 
