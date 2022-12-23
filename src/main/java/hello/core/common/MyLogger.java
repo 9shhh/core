9ddd88751e1764ed25098564391e13e91f105947
9shhh <uelj1234@naver.com>
2022-12-23 23:34:12 +0900

    request 요청에 대한 응답 테스트 - proxyMode = ScopedProxyMode.TARGET_CLASS 속성을 통한 프록시 방식으로 테스트

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,17 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+
+/**
+ * proxyMode = ScopedProxyMode.TARGET_CLASS 이라는 속성을 통해 CGLIB 라이브러리로 현재 클래스를 상속 받은 가짜 프록시 객체를 만들어 주입함.
+ * ScopedProxyMode.TARGET_CLASS 는 클래스, ScopedProxyMode.INTERFACE 는 인터페이스를 지원함. (맞는 형태로 사용하면 됨.)
+ * 실제 요청이 오면 그때 내부에서 실제 빈을 요청하는 위임 로직이 들어있음. (가짜 프록시 객체에.)
+ * 가짜 프록시 객체는 실제 request scope 와는 관계 없음. 그냥 가짜이고, 내부의 단순한 위임 로직만 있고 싱글톤 처럼 동작함.
+ * 해당 기능은 싱글톤 같지만 내부에서는 요청마다 전용 스코프가 생성되는 것을 주의해야함.
+ * 이런 특별한 스코프는 꼭 필요한 곳에서 최소화 해서 사용해야함. -> 무분별하게 사용하면 유지보수가 어려워짐.
+ *
+ */
 public class MyLogger { // 로그 출력을 위한 클래스
 
     private String uuid;
